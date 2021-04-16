bd307df1bd7d82edd38c2dfe524de8f6f03c44c1
hamzsait <hamzasait@utexas.edu>
2021-04-15 21:07:36 -0500

    added checks for blank entries

---
+++ README.md
@@ -0,0 +1,24 @@
+# Wasgoodindahood
+
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+  ## Description:
+  wasntgoodbefoabutbettanow
+  
+  ## Table of Contents
+  1. Title
+  2. Description
+  3. Installation
+  4. Usage
+  5. Lisence
+  6. Contributions
+  7. Tests
+  8. Questions
+
+  ## Installation Requirements
+  The following command will install the packages needed:
+
+    npm i hood
+  
+  
+  
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const markdown = require('./markdown.js')
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,31 +9,55 @@ const questions = [
     type: 'input',
     name: 'title',
     message: 'Enter a title for the repository:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
   },
   {
     type: 'input',
     name: 'description',
     message: 'Enter a description for the repository:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
   },
   {
     type: 'input',
     name: 'installationInstructions',
     message: 'Enter installation requirements:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Select liscences:',
+    choices: ['Apache', 'Boost', 'Eclipse','IBM','ISC','MIT','Mozilla','Perl']
   },
   {
     type: 'input',
     name: 'usageInformation',
     message: 'Enter usage information:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
   },
   {
     type: 'input',
-    name: 'contributionsGuidelines',
+    name: 'contributionGuidelines',
     message: 'Enter contribution guidelines:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
   },
   {
     type: 'input',
     name: 'testIntructions',
     message: 'Enter test Instructions:',
+    validate: ((userData) => {
+      return userData.length < 0 ? console.log('Please enter a valid entry.') : true
+    })
   },
 ]
 
@@ -47,7 +72,10 @@ const questions = [
 
 function init() {
   inquirer.prompt(questions).then(answers => {
-    markdown.generateMarkdown(answers)
+    data = markdown.generateMarkdown(answers)
+    fs.writeFile("README.md",data, (err) => {
+      console.log(err)
+    })
  })
 }
 
