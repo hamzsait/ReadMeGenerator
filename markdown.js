bee804a1ccdc7e20c26977c3416a06cfbc60873b
hamzsait <hamzasait@utexas.edu>
2021-04-15 21:02:02 -0500

    edited table of contents

--- markdown.js
+++ markdown.js
@@ -1,10 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  savedLicense = {
+    "Apache":"[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "Boost":"[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    "Eclipse":"[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+    "IBM":"[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
+    "ISC":"[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    "MIT":"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Mozilla":"[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    "Perl":"[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0",
+  }
+  output = ""
+  for (const lic of license){
+    output = output + savedLicense[lic]
+  }
+  return output
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,8 +30,29 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  //return `${data.title}`;
-  console.log(`${data.title}`)
+  return(
+  `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description:
+  ${data.description}
+  
+  ## Table of Contents
+  1. Installation
+  2. Usage
+  3. Lisence
+  4. Contributions
+  5. Tests
+  6. Questions
+
+  ## Installation Requirements
+  The following command will install the packages needed:
+
+    ${data.installationInstructions}
+  
+  
+  `)
 }
 
 module.exports = {
